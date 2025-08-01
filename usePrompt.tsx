ab40bc4a5467df3e65eecedd8d0b7cc49da8a993
Pallavi Amit Kumar <pallavizanje@gmail.com>
2025-08-01 19:25:46 +0530

    Update usePrompt.tsx

--- usePrompt.tsx
+++ usePrompt.tsx
@@ -1,7 +1,6 @@
 // hooks/usePrompt.ts
 import {
   unstable_useBlocker as useBlocker,
-  Transition,
 } from "react-router-dom";
 import { useEffect } from "react";
 
@@ -9,15 +8,13 @@ export const usePrompt = (message: string, when: boolean) => {
   const blocker = useBlocker(when);
 
   useEffect(() => {
-    if (!blocker.state === "blocked") return;
-
-    if (when && blocker.state === "blocked") {
+    if (blocker.state === "blocked") {
       const confirm = window.confirm(message);
       if (confirm) {
-        blocker.proceed(); // <- THIS is what navigates programmatically
+        blocker.proceed(); // ✅ allow navigation
       } else {
-        blocker.reset(); // cancel the transition
+        blocker.reset(); // ❌ cancel navigation
       }
     }
-  }, [blocker, when, message]);
+  }, [blocker, message, when]);
 };
