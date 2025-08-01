f4aa96ed4b4d4ee056a263273e55fe1c4d6f39aa
Pallavi Amit Kumar <pallavizanje@gmail.com>
2025-08-01 21:04:44 +0530

    Update FormNavigationGuard.tsx

--- FormNavigationGuard.tsx
+++ FormNavigationGuard.tsx
@@ -1,13 +1,11 @@
+// components/FormNavigationGuard.tsx
 import { useFormikContext } from "formik";
 import { usePrompt } from "../hooks/usePrompt";
 import { useBeforeUnload } from "../hooks/useBeforeUnload";
 
 export const FormNavigationGuard = ({ message = "You have unsaved changes. Leave anyway?" }) => {
-  const formik = useFormikContext();
-  const isDirty = formik.dirty;
-
-  usePrompt(message, isDirty);
-  useBeforeUnload(isDirty);
-
+  const { dirty } = useFormikContext();
+  usePrompt(message, dirty);
+  useBeforeUnload(dirty);
   return null;
 };
